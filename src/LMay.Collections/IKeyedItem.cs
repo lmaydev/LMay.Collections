e6961b9ed8f06c891dd7c1f9d50f322d2ffc66b7
L May <allinighdrui@gmail.com>
2022-01-18 13:45:33 +0000

    Added KeyedList and tests.

---
+++ src/LMay.Collections/IKeyedItem.cs
@@ -0,0 +1,14 @@
+﻿namespace LMay.Collections
+{
+    /// <summary>
+    /// Represents an item that exposes it's own key through a property.
+    /// </summary>
+    /// <typeparam name="TKey">The type of the key.</typeparam>
+    public interface IKeyedItem<TKey>
+    {
+        /// <summary>
+        /// Gets the key for this item.
+        /// </summary>
+        TKey Key { get; }
+    }
+}
